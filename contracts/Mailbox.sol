14f438059f0c15e0c975efca09d5aa696f1b637a
william godard <grenier.godard@gmail.com>
2022-03-06 00:31:59 +0100

    add openzeppelin lib

--- contracts/Mailbox.sol
+++ contracts/Mailbox.sol
@@ -1,34 +1,27 @@
-pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 
-contract Mailbox {
-
-  event RegisterUser(address sender, string mailboxStorage);
-
-  string public purpose = "Building Unstoppable Apps!!!";
+contract Mailbox is Context {
   mapping(address => string) private mailboxes;
 
+  event RegisterUser(address sender, string mailboxStorage);
+  
   constructor() {
     // what should we do on deploy?
   }
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
+  function getUserInbox() external view returns (string memory) {
+    console.log(mailboxes[_msgSender()]);
+    return mailboxes[_msgSender()];
   }
 
-
-  function getUserInbox(address user) public view returns (string memory) {
-    console.log(mailboxes[user]);
-    return mailboxes[user];
-  }
-
-  function registerUser(string memory mailboxStorage) public {
-      mailboxes[msg.sender] = mailboxStorage;
-      console.log(msg.sender, mailboxStorage);
-      emit RegisterUser(msg.sender, mailboxStorage);
+  function registerUser(string memory mailboxStorage) external {
+      mailboxes[_msgSender()] = mailboxStorage;
+      console.log(_msgSender(), mailboxStorage);
+      emit RegisterUser(_msgSender(), mailboxStorage);
   }
 
   // to support receiving ETH by default
