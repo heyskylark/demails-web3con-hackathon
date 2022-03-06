ee7dbe3a3ccdbc2bf166b16d51d780030537e0c8
Brandon Feist <brandon.feist@hey.com>
2022-03-05 16:09:33 -0800

    Setup work for inbox init and sending emails

--- src/context/orbitDbContext.js
+++ src/context/orbitDbContext.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext, createContext } from "react";
-import { useEthersProvider } from "../context/providerContext.js";
+import { useEthersProvider } from "./providerContext.js";
 import Identities from "orbit-db-identity-provider";
 
 const IPFS = require("ipfs-http-client");
@@ -37,6 +37,7 @@ function useProvideOrbitDb() {
   const provider = useEthersProvider();
   const [ipfs, setIpfs] = useState(null);
   const [orbitDb, setOrbitDb] = useState(null);
+  const [inbox, setInbox] = useState(null);
 
   useEffect(() => {
     function cleanup() {
@@ -81,15 +82,28 @@ function useProvideOrbitDb() {
     return cleanup();
   }, [provider.signer]);
 
-  async function fetchOrInitInbox() {
+  async function fetchInbox() {
     if (provider.signer && orbitDb) {
       // TODO: smart contract to check if for existing wallet
       // orbitDb.open("<addr-from-smrt-ctrt>")
-      //   .then((inboxDb) => { inboxDb.load() ... <other logic>})
+      //   .then((inboxDb) => { setInbox(inboxDb); inboxDb.load(); ... <other logic>})
       //   .catch((err) => {/** Some problem occured, I think this will happen if no DB is found, create-db */})
+    } else {
+      console.log("User's wallet is not connected");
+    }
+  }
+
+  async function initInbox() {
+    if (provider.signer) {
+      if (inbox || (await fetchInbox())) {
+        console.log("User already has an inbox");
+        return;
+      }
+
       const address = await provider.signer.getAddress();
       const userInbox = await orbitDb.docs(address);
       // TODO: update smart contract with OrbitDb database address: userInbox.address.toString()
+      // TODO: move pending emails from pending email DB to new DB
 
       return userInbox;
     } else {
@@ -97,7 +111,44 @@ function useProvideOrbitDb() {
     }
   }
 
+  /*
+  * Need to figure out email body. Is it possible to have types to adhear to in regular JS like TypeScript?
+  {
+    id: <hash of all the fields below>
+    from: <wallet address>
+    to: list(<wallet address>)
+    signedMessage: <message signed by personal_sign> *** this can be added in this function below ***
+    originalMessage: <message before it was signed by personal_sign> *** this can be added in this function below ***
+    subject: <subject string>
+    body: <body string>
+    createdAt: Date.now()
+  }
+  */
+  async function sendEmail(email) {
+    // User needs an init inbox before sending emails
+    if (provider.signer) {
+      if (!inbox) {
+        console.log("User must have an inbox before sending emails");
+        return;
+      }
+
+      const toEmails = email.to;
+      for (let emailIndex = 0; emailIndex < toEmails.length; emailIndex++) {
+        // TODO: Fetch orbitDb inbox address from smart contract toEmails[emailIndex]
+        // TODO: if no orbitDb address for email.to, fetch pendingEmails DB address from smart contract
+        // TODO: save email to correct DB address
+        // orbitDb.open(recievingInboxAddr)
+        // .then((recievingInbox) => { recievingInbox.put(email) ... <other logic> ... recievingInbox.close() })
+        // .catch((err) => {/** Some problem occured, I think this will happen if no DB is found, email not sent */})
+      }
+    } else {
+      console.log("User's wallet must be connected to send email.");
+    }
+  }
+
   return {
-    fetchOrInitInbox,
+    fetchInbox,
+    initInbox,
+    sendEmail,
   };
 }
