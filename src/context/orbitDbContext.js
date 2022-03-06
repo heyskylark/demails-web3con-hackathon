fc60a1a57c4b762206eb6b251fcb6f8338f4d5b8
Brandon Feist <brandon.feist@hey.com>
2022-03-06 00:13:24 -0800

    Fix merge conflicts

--- src/context/orbitDbContext.js
+++ src/context/orbitDbContext.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 import * as MailboxJson from "../artifacts/Mailbox.json";
-=======
->>>>>>> 1950ff9b2b980201c0139ce440ea62d3d3d0b8e7
 import { useState, useEffect, useContext, createContext } from "react";
 import { useEthersProvider } from "./providerContext.js";
 import Identities from "orbit-db-identity-provider";
 
-<<<<<<< HEAD
 const { abi } = MailboxJson;
-=======
->>>>>>> 1950ff9b2b980201c0139ce440ea62d3d3d0b8e7
 const IPFS = require("ipfs-http-client");
 const OrbitDB = require("orbit-db");
 
@@ -43,7 +37,6 @@ const ipfsOptions = {
 
 function useProvideOrbitDb() {
   const provider = useEthersProvider();
-<<<<<<< HEAD
   // TODO: Move this to dotenv file
   const mailboxContractAddr = "0x8b97C01F447537d9403Eb7eA9395f63a0f59361D";
   const contractABI = abi;
@@ -53,11 +46,6 @@ function useProvideOrbitDb() {
   const [inbox, setInbox] = useState(null);
   const [pendingMailbox, setPendingMailbox] = useState();
   const [mailboxContract, setMailboxContract] = useState(null);
-=======
-  const [ipfs, setIpfs] = useState(null);
-  const [orbitDb, setOrbitDb] = useState(null);
-  const [inbox, setInbox] = useState(null);
->>>>>>> 1950ff9b2b980201c0139ce440ea62d3d3d0b8e7
 
   useEffect(() => {
     function cleanup() {
@@ -95,7 +83,6 @@ function useProvideOrbitDb() {
         .catch((err) => {
           console.log("There was a problem getting the OrbitDb identity", err);
         });
-<<<<<<< HEAD
 
       const contract = provider.connectToContract(
         mailboxContractAddr,
@@ -105,8 +92,6 @@ function useProvideOrbitDb() {
       contract.pendingInbox().then((pendingMailboxAddr) => {
         setPendingMailbox(pendingMailboxAddr);
       });
-=======
->>>>>>> 1950ff9b2b980201c0139ce440ea62d3d3d0b8e7
     } else {
       cleanup();
     }
@@ -114,7 +99,6 @@ function useProvideOrbitDb() {
     return cleanup();
   }, [provider.signer]);
 
-<<<<<<< HEAD
   useEffect(() => {
     if (provider.addr && provider.signer && orbitDb) {
       fetchInbox(provider.addr, true);
@@ -139,36 +123,21 @@ function useProvideOrbitDb() {
             });
         }
       });
-=======
-  async function fetchInbox() {
-    if (provider.signer && orbitDb) {
-      // TODO: smart contract to check if for existing wallet
-      // orbitDb.open("<addr-from-smrt-ctrt>")
-      //   .then((inboxDb) => { setInbox(inboxDb); inboxDb.load(); ... <other logic>})
-      //   .catch((err) => {/** Some problem occured, I think this will happen if no DB is found, create-db */})
->>>>>>> 1950ff9b2b980201c0139ce440ea62d3d3d0b8e7
     } else {
       console.log("User's wallet is not connected");
     }
   }
 
-<<<<<<< HEAD
   // Useful DocStore documentation: https://github.com/orbitdb/orbit-db-docstore
   async function initInbox() {
     if (provider.signer) {
       if (inbox) {
-=======
-  async function initInbox() {
-    if (provider.signer) {
-      if (inbox || (await fetchInbox())) {
->>>>>>> 1950ff9b2b980201c0139ce440ea62d3d3d0b8e7
         console.log("User already has an inbox");
         return;
       }
 
       const address = await provider.signer.getAddress();
       const userInbox = await orbitDb.docs(address);
-<<<<<<< HEAD
       const inboxAddr = userInbox.address.toString();
       mailboxContract
         .addInbox(inboxAddr)
@@ -181,12 +150,6 @@ function useProvideOrbitDb() {
           userInbox.drop();
           console.log("There was a problem initializing the inbox", err);
         });
-=======
-      // TODO: update smart contract with OrbitDb database address: userInbox.address.toString()
-      // TODO: move pending emails from pending email DB to new DB
-
-      return userInbox;
->>>>>>> 1950ff9b2b980201c0139ce440ea62d3d3d0b8e7
     } else {
       console.log("User's wallet is not connected");
     }
@@ -195,11 +158,7 @@ function useProvideOrbitDb() {
   /*
   * Need to figure out email body. Is it possible to have types to adhear to in regular JS like TypeScript?
   {
-<<<<<<< HEAD
     _id: <hash of all the fields below> (Warning: ID field must be _id as that's what OrbitDb defaults to for id field)
-=======
-    id: <hash of all the fields below>
->>>>>>> 1950ff9b2b980201c0139ce440ea62d3d3d0b8e7
     from: <wallet address>
     to: list(<wallet address>)
     signedMessage: <message signed by personal_sign> *** this can be added in this function below ***
@@ -229,7 +188,6 @@ function useProvideOrbitDb() {
 
       const toEmails = email.to;
       for (let emailIndex = 0; emailIndex < toEmails.length; emailIndex++) {
-<<<<<<< HEAD
         const toAddr = toEmails[emailIndex];
         const recievingInboxAddr =
           (await mailboxContract.getInbox(toAddr)) || pendingMailbox;
@@ -243,14 +201,6 @@ function useProvideOrbitDb() {
           .catch((err) => {
             console.log("Inbox not found, email not sent for" + toAddr, err);
           });
-=======
-        // TODO: Fetch orbitDb inbox address from smart contract toEmails[emailIndex]
-        // TODO: if no orbitDb address for email.to, fetch pendingEmails DB address from smart contract
-        // TODO: save email to correct DB address
-        // orbitDb.open(recievingInboxAddr)
-        // .then((recievingInbox) => { recievingInbox.put(email) ... <other logic> ... recievingInbox.close() })
-        // .catch((err) => {/** Some problem occured, I think this will happen if no DB is found, email not sent */})
->>>>>>> 1950ff9b2b980201c0139ce440ea62d3d3d0b8e7
       }
     } else {
       console.log("User's wallet must be connected to send email.");
@@ -262,10 +212,7 @@ function useProvideOrbitDb() {
   // TODO: filtering spoofed emails when inbox is query when first connected and also when new emails come in through events
 
   return {
-<<<<<<< HEAD
     inbox,
-=======
->>>>>>> 1950ff9b2b980201c0139ce440ea62d3d3d0b8e7
     fetchInbox,
     initInbox,
     sendEmail,
