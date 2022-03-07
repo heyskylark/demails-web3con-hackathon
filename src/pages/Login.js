da3daf1b0c2c61afc8ac98ac1da7634d17310d7c
Brandon Feist <brandon.feist@hey.com>
2022-03-06 20:50:23 -0800

    clean up unused things

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -28,7 +28,6 @@ Type.propTypes = { text: PropTypes.string };
 function Login() {
   const provider = useEthersProvider();
   const orbitDb = useOrbitDb();
-  const [flag, setFlag] = React.useState(false);
 
   function walletConnectComponent() {
     return (
@@ -64,39 +63,7 @@ function Login() {
       );
     }
   }
-  const handleRequestPersonalSign = React.useCallback(async () => {
-    const res = await provider.requestPersonalSign();
-    console.log(res);
-    console.log(window);
-    if (res) {
-      const isValidSignature = await provider.validatePersonalSign(res[2], res[0], res[1]);
-      console.log(isValidSignature);
-      if (isValidSignature) {
-        console.log(window);
-        setFlag(true);
-        renderLogin(true);
-        // eslint-disable-next-line
-        sessionStorage.setItem("lastSessionSignedIn", true);
-      }
-    }
-    setFlag(false);
-  }, [provider]);
 
-  function connectedComponent() {
-    const inboxAddr = orbitDb.inboxAddr ? orbitDb.inboxAddr : "None";
-    return (
-      <>
-        <OpenRouteTemplate>
-          <h1>
-            Hi,Activate the Account ! <span className="wave">👋🏻</span>{" "}
-          </h1>
-          <h4 type="secondary">
-            <Type text="To activate Mail, you will need to sign a message through metamask." />
-          </h4>
-        </OpenRouteTemplate>
-      </>
-    );
-  }
   function renderLogin() {
     if (!orbitDb.inbox) {
       return initInboxButton();
